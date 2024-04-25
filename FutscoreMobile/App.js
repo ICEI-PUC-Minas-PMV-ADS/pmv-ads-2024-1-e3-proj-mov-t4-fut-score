3ad2e1a1ded0973d20f9f282ce8c06870c0398bd
Caio Santos <mrlose22@gmail.com>
2024-04-24 21:57:02 -0300

    Testando commit

--- FutscoreMobile/App.js
+++ FutscoreMobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ola Mundo</Text>
+      <Text>Oala Mundo 2</Text>
       <StatusBar style="auto" />
     </View>
   );
