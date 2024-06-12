9e2aea80f0434dc212301aef669c246859ead512
IzzatCarlos <carloskakaa0@gmail.com>
2024-06-12 01:27:36 -0300

    escudos e format

--- FutscoreMobile/src/Pages/SerieA.js
+++ FutscoreMobile/src/Pages/SerieA.js
@@ -3,26 +3,26 @@ import { View, StyleSheet, FlatList, Image, Text } from 'react-native';
 import { DataTable } from 'react-native-paper';
 
 const SerieA = [
-  { team: 'Atlético Paranaense', games: 6, wins: 4, draws: 1, losses: 1, points: 13, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/q9fhEsgpuyRq58OgmSndcQ_24x24.png' },
-  { team: 'Bahia', games: 6, wins: 4, draws: 1, losses: 1, points: 13, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/orE554NToSkH6nuwofe7Yg_24x24.png' },
-  { team: 'Flamengo', games: 6, wins: 3, draws: 2, losses: 1, points: 11, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/orE554NToSkH6nuwofe7Yg_24x24.png' },
-  { team: 'Botafogo', games: 6, wins: 3, draws: 1, losses: 2, points: 10, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/j3lRjFqECj0waFZfnsyg1A_24x24.png' },
-  { team: 'São Paulo', games: 6, wins: 3, draws: 1, losses: 2, points: 10, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/s5TClOZN5-dz5xUeMub_Lw_24x24.png' },
-  { team: 'Cruzeiro', games: 5, wins: 3, draws: 1, losses: 1, points: 10, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/Tcv9X__nIh-6wFNJPMwIXQ_48x48.png' },
-  { team: 'Atlético-MG', games: 5, wins: 2, draws: 3, losses: 0, points: 9, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/j3lRjFqECj0waFZfnsyg1A_96x96.png' },
-  { team: 'RB Bragantino', games: 6, wins: 2, draws: 3, losses: 1, points: 9, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/j3lRjFqECj0waFZfnsyg1A_96x96.png' },
-  { team: 'Palmeiras', games: 6, wins: 2, draws: 2, losses: 2, points: 8, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/q9fhEsgpuyRq58OgmSndcQ_96x96.png' },
-  { team: 'Internacional', games: 4, wins: 2, draws: 1, losses: 1, points: 7, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/nXi3Tkm-oLpXh9yMzvZwFQ_96x96.png' },
-  { team: 'Fortaleza EC', games: 5, wins: 1, draws: 4, losses: 0, points: 7, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/me10ephzRxdj45zVq1Risg_96x96.png' },
-  { team: 'Grêmio', games: 4, wins: 2, draws: 0, losses: 2, points: 6, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/Ku-73v_TW9kpex-IEGb0ZA_96x96.png' },
-  { team: 'Vasco da Gama', games: 6, wins: 2, draws: 0, losses: 4, points: 6, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/2tLgBzJNpWhzwIqkNb8nBQ_96x96.png' },
-  { team: 'Criciúma', games: 3, wins: 1, draws: 2, losses: 0, points: 5, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/BChN4yf3zwyiMkj5xL4pIg_96x96.png' },
-  { team: 'Juventude', games: 4, wins: 1, draws: 2, losses: 1, points: 5, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/2tLgBzJNpWhzwIqkNb8nBQ_96x96.png' },
+  { team: 'Alético Paranaense', games: 6, wins: 4, draws: 1, losses: 1, points: 13, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/q9fhEsgpuyRq58OgmSndcQ_96x96.png' },
+  { team: 'Bahia', games: 6, wins: 4, draws: 1, losses: 1, points: 13, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/nIdbR6qIUDyZUBO9vojSPw_48x48.png' },
+  { team: 'Flamengo', games: 6, wins: 3, draws: 2, losses: 1, points: 11, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/orE554NToSkH6nuwofe7Yg_48x48.png' },
+  { team: 'Botafogo', games: 6, wins: 3, draws: 1, losses: 2, points: 10, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/KLDWYp-H8CAOT9H_JgizRg_48x48.png' },
+  { team: 'São Paulo', games: 6, wins: 3, draws: 1, losses: 2, points: 10, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/4w2Z97Hf9CSOqICK3a8AxQ_48x48.png' },
+  { team: 'Cruzeiro', games: 5, wins: 3, draws: 1, losses: 1, points: 10, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/Tcv9X__nIh-6wFNJPMwIXQ_96x96.png' },
+  { team: 'Atético-MG', games: 5, wins: 2, draws: 3, losses: 0, points: 9, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/q9fhEsgpuyRq58OgmSndcQ_48x48.png' },
+  { team: 'RB Bragantino', games: 6, wins: 2, draws: 3, losses: 1, points: 9, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/lMyw2zn1Z4cdkaxKJWnsQw_48x48.png' },
+  { team: 'Palmeiras', games: 6, wins: 2, draws: 2, losses: 2, points: 8, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/7spurne-xDt2p6C0imYYNA_48x48.png' },
+  { team: 'Internacional', games: 4, wins: 2, draws: 1, losses: 1, points: 7, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/OWVFKuHrQuf4q2Wk0hEmSA_48x48.png' },
+  { team: 'Fortaleza EC', games: 5, wins: 1, draws: 4, losses: 0, points: 7, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/me10ephzRxdj45zVq1Risg_48x48.png' },
+  { team: 'Grêmio', games: 4, wins: 2, draws: 0, losses: 2, points: 6, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/Ku-73v_TW9kpex-IEGb0ZA_48x48.png' },
+  { team: 'Vasco da Gama', games: 6, wins: 2, draws: 0, losses: 4, points: 6, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/hHwT8LwRmYCAGxQ-STLxYA_48x48.png' },
+  { team: 'Criciúma', games: 3, wins: 1, draws: 2, losses: 0, points: 5, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/u_L7Mkp33uNmFTv3uUlXeQ_48x48.png' },
+  { team: 'Juventude', games: 4, wins: 1, draws: 2, losses: 1, points: 5, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/JrXw-m4Dov0gE2Sh6XJQMQ_48x48.png' },
   { team: 'Corinthians', games: 6, wins: 1, draws: 2, losses: 3, points: 5, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/tCMSqgXVHROpdCpQhzTo1g_48x48.png' },
-  { team: 'Fluminense', games: 6, wins: 1, draws: 2, losses: 3, points: 5, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/edrB5RlpgImexYTh2nklPA_96x96.png' },
-  { team: 'Vitória', games: 5, wins: 0, draws: 1, losses: 4, points: 1, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/_UukGr-o3S9ET5J8F32Xhg_96x96.png' },
-  { team: 'Atlético Goianiense', games: 5, wins: 0, draws: 1, losses: 4, points: 1, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/q9fhEsgpuyRq58OgmSndcQ_96x96.png' },
-  { team: 'Cuiabá', games: 4, wins: 0, draws: 0, losses: 4, points: 0, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/q9fhEsgpuyRq58OgmSndcQ_96x96.png' },
+  { team: 'Fluminense', games: 6, wins: 1, draws: 2, losses: 3, points: 5, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/fCMxMMDF2AZPU7LzYKSlig_48x48.png' },
+  { team: 'Vitória', games: 5, wins: 0, draws: 1, losses: 4, points: 1, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/LHSM6VchpkI4NIptoSTHOg_48x48.png' },
+  { team: 'Atlético Goianiense', games: 5, wins: 0, draws: 1, losses: 4, points: 1, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/9mqMGndwoR9og_Z0uEl2kw_48x48.png' },
+  { team: 'Cuiabá', games: 4, wins: 0, draws: 0, losses: 4, points: 0, logoUrl: 'https://ssl.gstatic.com/onebox/media/sports/logos/j6U8Rgt_6yyf0Egs9nREXw_48x48.png' },
 ];
 
 const StatsTable = () => {
@@ -43,7 +43,7 @@ const StatsTable = () => {
           renderItem={({ item }) => (
             <DataTable.Row>
               <DataTable.Cell style={styles.teamCell}>
-                <Image style={styles.logo} source={{ uri: item.logoUrl }} />
+                <Image style={styles.logo} source={{ uri: item.logoUrl }} onError={() => console.log(`Error loading image for ${item.team}`)} />
                 <Text>{item.team}</Text>
               </DataTable.Cell>
               <DataTable.Cell numeric>{item.games}</DataTable.Cell>
@@ -68,11 +68,12 @@ const styles = StyleSheet.create({
   teamCell: {
     flexDirection: 'row',
     alignItems: 'center',
+    flex: 3,
   },
   logo: {
-    width: 28,
-    height: 28,
-    marginRight: 6,
+    width: 14,
+    height: 14,
+    marginRight: 5,
   },
 });
 
